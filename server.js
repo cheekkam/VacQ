2a8fd6a393655b5108136d369570b674d706b801
Thapanee <kamboonchob@gmail.com>
2022-04-02 23:25:18 +0700

    OpenAPI with Swagger

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 // Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -40,7 +42,7 @@ app.use(xss());
 // Rate Limiting
 const limiters = rateLimit({
     windowsMs: 10*60*1000, // 10 mins
-    max: 1
+    max: 10
 });
 app.use(limiters);
 
@@ -54,6 +56,25 @@ app.use(cors());
 app.use('/api/v1/hospitals', hospitals);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/appointments', appointments);
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ],
+    },
+    apis: ['./routes/*.js'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 const PORT=process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
